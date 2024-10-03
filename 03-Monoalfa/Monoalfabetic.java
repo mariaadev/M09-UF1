f01a1671317e9b91b707454fc3fda09346600704
Maria Amzil <mini.mar@hotmail.com>
2024-10-03 16:24:48 +0200

    desxifrar and xifrar work

--- 03-Monoalfa/Monoalfabetic.java
+++ 03-Monoalfa/Monoalfabetic.java
@@ -6,11 +6,16 @@ public class Monoalfabetic {
     public static ArrayList<Character> abc = new ArrayList<Character>();
     public static ArrayList<Character> abcPermutat = new ArrayList<Character>();
     public static void main(String[] args) {
+        String cadenaxifrada;
+        String cadenadesxifrada;
         inicialitzarLlista();
         permutaAlfabet(abcPermutat);
         System.out.println(abc);
         System.out.println(abcPermutat);
-        System.out.println(xifraMonoAlfa("hola"));
+        cadenaxifrada = xifraMonoAlfa("hola");
+        System.out.println(cadenaxifrada);
+        cadenadesxifrada = desxifraMonoAlfa(cadenaxifrada);
+        System.out.println(cadenadesxifrada);
     }
 
     public static void inicialitzarLlista() {
@@ -56,7 +61,31 @@ public class Monoalfabetic {
   
         
     public static String desxifraMonoAlfa(String cadena) {
-        return "";
+        StringBuilder cadenadesxifrada = new StringBuilder();
+        for (int i = 0; i < cadena.length(); i++) {
+            char lletra = cadena.charAt(i);
+            //index lletra cadena
+            int index = abcPermutat.indexOf(lletra);
+
+            if (index == -1) {
+                lletra = Character.toLowerCase(lletra);
+
+                index = abcPermutat.indexOf(lletra);
+                if (index == -1) {
+                    //simbol
+                    cadenadesxifrada.append(cadena.charAt(i));
+                    break;
+                } 
+                
+                cadenadesxifrada.append( (Character)(abc.get(index))) ;
+              
+            } else {
+                cadenadesxifrada.append(abc.get(index));
+            }
+            //identifica l'index de la lletra i mirar si es min o maj
+            //reemplazar por el indice encontrado de la letra pero del array permutado
+       }
+       return cadenadesxifrada.toString();
     }
 
 }

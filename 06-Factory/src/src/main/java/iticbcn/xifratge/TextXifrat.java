826423f005b284b8238d8744e3ff40fbe8fcb614
Maria Amzil <mini.mar@hotmail.com>
2024-11-05 12:28:34 +0100

    adding implementation to TextXifrat

--- 06-Factory/src/src/main/java/iticbcn/xifratge/TextXifrat.java
+++ 06-Factory/src/src/main/java/iticbcn/xifratge/TextXifrat.java
@@ -1,14 +1,22 @@
 package iticbcn.xifratge;
 
-public class TextXifrat {
+import java.nio.charset.StandardCharsets;
 
-    
-    @Override
-    public String toString() {
-        return "";
+public class TextXifrat {
+    private byte[] bytes;
+
+    public TextXifrat (byte[] bytes) {
+        this.bytes = bytes;
     }
 
-    /*public byte[] getBytes() {
-        
-    }*/
+    public byte[] getBytes() {
+        return this.bytes;
+    }
+
+    @Override
+    public String toString() {
+        /*convertir array de bytes a String amb l'estàndar utf8 */
+        return new String(this.bytes, StandardCharsets.UTF_8);
+    }
+
 }

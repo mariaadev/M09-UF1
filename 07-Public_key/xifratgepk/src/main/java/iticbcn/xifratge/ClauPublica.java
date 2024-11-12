a9ed23b93cc02e8e0d5aa1856f71e226284abe3a
Maria Amzil <mini.mar@hotmail.com>
2024-11-12 19:19:37 +0100

    adding ClauPublica class and methods

---
+++ 07-Public_key/xifratgepk/src/main/java/iticbcn/xifratge/ClauPublica.java
@@ -0,0 +1,13 @@
+package iticbcn.xifratge;
+
+public class ClauPublica {
+    public KeyPair generaParellClausRSA() throws Exception {
+
+    }
+    public byte[] xifraRSA(String msg, PublicKey ClauPublica) throws Exception {
+        
+    }
+    public String desxifraRSA(byte[] msgXifrat, PrivateKey ClauPrivada) {
+        
+    }
+}
